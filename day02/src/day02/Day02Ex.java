b0ee8e6d897ef07c8399aa0881a903551ad729e0
minnnjuuu <kmj020411@naver.com>
2023-05-08 21:16:04 +0900

    2강. 출력 메서드와 입력 메서드

---
+++ day02/src/day02/Day02Ex.java
@@ -0,0 +1,28 @@
+package day02;
+
+public class Day02Ex {
+
+	public static void main(String[] args) {
+		//안녕하세요
+		System.out.print("hello world!");
+		System.out.print("문자열 값"); //문자열 값
+		System.out.print("A");      //문자형 값
+		System.out.print(100);      //정수형 값
+		System.out.print(3.14);     //실수형 값
+		
+		
+		System.out.println("문자열 값"); //문자열 값
+		System.out.println("A");      //문자형 값
+		System.out.println(100);      //정수형 값
+		System.out.println(3.14);     //실수형 값
+		//sysout 커서 뒤에 Ctrl + Space 누르면 해당 메서드 자동완성
+		
+		
+		//format : 형식 지정
+		System.out.printf("%s","문자열 값"); //문자열 값
+		System.out.printf("%c",'A');      //문자형 값
+		System.out.printf("%d",100);      //정수형 값
+		System.out.printf("%f",3.14);     //실수형 값
+	}
+
+}
---
+++ day02/src/day02/Day02_input.java
@@ -0,0 +1,24 @@
+package day02;
+//입력 기능을 내코드에 연결
+import java.util.Scanner;
+public class Day02_input {
+
+	public static void main(String[] args) {
+		// 스캐너를 sc 로 사용
+		Scanner sc = new Scanner(System.in);
+		//문자열로 입력 -> 기능자체가 키보드로 입력한 값
+		System.out.println(sc.next());
+		//정수값 입력
+		System.out.println(sc.nextInt());
+		//실수값으로 입력
+		System.out.println(sc.nextFloat());
+		//띄어쓰기 전까지 출력
+		System.out.println(sc.nextLine());
+
+
+
+		
+		
+	}
+
+}
