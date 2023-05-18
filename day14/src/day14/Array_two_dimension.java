81b6f267477052c8b2d195db61ecdc9dd4eeb8cd
minnnjuuu <kmj020411@naver.com>
2023-05-18 16:52:24 +0900

    14강. 다차원 배열

---
+++ day14/src/day14/Array_two_dimension.java
@@ -0,0 +1,15 @@
+package day14;
+
+public class Array_two_dimension {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//5개의 정수형 변수 묶는 1차원 배열 x2 = 2차원 배열
+		int ar1[]= new int [5];
+		int ar2[]= new int [5];
+		
+		//자료형 배열명[][]=new int[묶음크기][변수개수];
+		int ar3[][]= new int [2][5];
+	}
+
+}
