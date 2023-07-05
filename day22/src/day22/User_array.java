e3eef05101940156642a77f827216d902ed6763d
김민주 <kmj020411@naver.com>
2023-07-05 19:48:37 +0900

    22강.클래스-객체타입배열/연습계산기

---
+++ day22/src/day22/User_array.java
@@ -0,0 +1,44 @@
+package day22;
+
+import java.util.Scanner;
+
+public class User_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		User users[] = new User[4];
+		for(int i=0;i<4;i++) {
+			users[i] = new User();
+		}
+		for (int i=0;i<4;i++) {
+			System.out.print("name : ");
+			users[i].name = sc.next();
+			System.out.print("phone : ");
+			users[i].phone = sc.next();	
+			System.out.print("age : ");
+			users[i].age = sc.nextInt();
+			System.out.print("gender : ");
+			users[i].gender = sc.next();
+		}
+		for(int i=0;i<4;i++) {
+			users[i].info();
+		}
+	}
+
+}
+
+class User{
+	String name;
+	String phone;
+	int age;
+	String gender;
+	 void info() {
+		 System.out.println("name : "+name);
+		 System.out.println("phone : "+phone);
+		 System.out.println("age : "+age);
+		 System.out.println("gender : "+gender);
+
+		 
+	 }
+}
